a0a5a4d34589ffe4b3891b7e04dd9fba20eee3fe
TheLeftyCoder <aashutoshmishra296@gmail.com>
2025-08-15 21:54:37 +0530

    exploring react element and component

--- App.js
+++ App.js
@@ -5,28 +5,3 @@ const heading = React.createElement(
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
-/**  <div id="parent">
-        <div id="child">
-       <h1> I am h1 tag </h1>
-       <h2> I am h2 tag </h2>
-        </div>
-        <div id="child 2">
-       <h1> I am h1 tag </h1>
-       <h2> I am h2 tag </h2>
-        </div>
-        </div> 
-**/
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-    React.createElement("div", { id: "child 2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
-root.render(parent);

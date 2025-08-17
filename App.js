5ae5154e68aff665053643d476c127bd04c795ae
TheLeftyCoder <aashutoshmishra296@gmail.com>
2025-08-17 23:17:50 +0530

    used component composition

--- App.js
+++ App.js
@@ -1,37 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTML Element (render this to DOM then it becomes HTML element)
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Namaste React!!!🚀"
-// );
-// console.log(heading);
+/**
+* Header
+*  - Logo
+*  - Nav Items
+* Body
+*  - Search Bar
+*  - RestaurantContainer
+*    - RestaurantCard
+* Footer
+*  - Copyright
+*  - Links
+*  - Address
+*  - Contact
+*/
 
-//JSX - Is not HTML in js , it is HTML or XML like syntax
-//JSX (transpiled before it reaches the js) and the conversion is done by Babel
-//JSX is converted in React.createElement and this react.createElement makes a react element(JS object) which is then rendered to the DOM as an HTML element
-// React Element
-const heading = <h1 className="head">Namaste React using jsx!!!🚀</h1>;// and react component is written like this (const Heading = ()=> )
 
-// React Component - There is two types of components:
-// 1- Class based component - Old way of writing code
-// 2- Functional component - New way of writing code, more popular and recommended
-
-// React functional component (A function that returns JSX code is a functional component)
-const HeadingComponent = () => {
+// React component
+function Title() {
   return (
-    <h1 className="heading">Namaste React using functional component!!!🚀</h1>
+    <h1 className="head" tabIndex="5">
+      Namaste I am Arya
+    </h1>
   );
-};
-//OR (this heading component can be written like as given below)
+}
+
+// React element
+const title = (
+  <h1 className="head" tabIndex="5">
+    Namaste I am Arya mishra and this is a React element
+  </h1>
+);
+
+// Component composition
 const HeadingComponent2 = () => (
-    <div id="container">
-  <h1 className="heading 2">Namaste React using functional component!!!🚀</h1>
+  <div id="container">
+    <Title /> {/* We can use a component like this */}
+    <Title></Title> {/* this three Title component are same */}
+    {Title()}  {/* We can use this component as an function also */}
+
+    {/* We can use a Element like this {title} */}
+    
+    <h2>{100 + 200}</h2> {/*Writing JS expression inside JSX*/}
+    <h1 className="heading 2">Namaste React using functional component!!!🚀</h1>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>); // We render functional component like this using </>
+root.render(<HeadingComponent2 />); // We render functional component like this using </>
 // root.render(heading); // We render react element like this

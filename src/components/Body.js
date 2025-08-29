1b4707f0ba31ee409343e26d81b3dcbe298e245c
TheLeftyCoder <aashutoshmishra296@gmail.com>
2025-08-30 01:30:23 +0530

    made shimmer ui and login button and updated filtered restaurant feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,64 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resObj from "../utils/cart";
-
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  const [listOfRestaurant,setListOfRestaurant] = useState(resObj);
+  const [searchText, setSearchText] = useState("");
 
-  return (
+  // Whenever state variables update , react triggers a reconciliation cycle (re-renders component) xx
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6275609&lng=77.2784081&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  // Conditional rendering - (Rendering on the basis of condition is known as conditional rendering)
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //Filter the restaurant cards and update the UI
+              //search text
+              const filteredRestaurant = listOfRestaurant.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setListOfRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -21,8 +70,8 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.id} resData={restaurant} />
+        {filteredRestaurant?.map((restaurant, idx) => (
+          <RestaurantCard key={idx} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,20 +1,20 @@
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { name, cuisine, rating, costForTwo, image } = resData;
-
+  const { name, cuisines, avgRatingString, costForTwo, image, cloudinaryImageId} = resData.info;
+  const imageCdn = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="res-logo"
         alt="res-logo"
-        src={image}
+        src={imageCdn + cloudinaryImageId}
         width={50}
         height={100}
       />
       <h3>{name}</h3>
-      <h4>{cuisine.join(",")}</h4>
-      <h4>{rating}stars</h4>
-      <h4>₹ {costForTwo / 10} For two</h4>
+      <h4>{cuisines.join(",")}</h4>
+      <h4>{avgRatingString}stars</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
